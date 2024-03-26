56b0059473d5aa290ce47aa309d0f58b3828f254
Christian McDuffy <csduffy1@gmail.com>
2024-03-26 00:23:55 -0400

    Update DataCleaningIn.sql

--- DataCleaningIn.sql
+++ DataCleaningIn.sql
@@ -1,4 +1,4 @@
-SELECT * FROM `Excel Bikes`.`excel project dataset 2 copy`;
+SELECT * FROM `Excel Bikes`.`excel project dataset 2 copy`; -- select all data
 SELECT 
     CASE 
         WHEN Gender = 'M' THEN 'Male'
@@ -6,7 +6,7 @@ SELECT
         ELSE Gender
     END AS Gender_Converted,
     Gender
-FROM `Excel Bikes`.`excel project dataset 2 copy`;
+FROM `Excel Bikes`.`excel project dataset 2 copy`;  -- Create new column and insert Gender column variables as 'Male' & 'Female
 
 UPDATE `Excel Bikes`.`excel project dataset 2 copy`
 SET Gender = 
@@ -14,27 +14,26 @@ SET Gender =
         WHEN Gender = 'M' THEN 'Male'
         WHEN Gender = 'F' THEN 'Female'
         ELSE Gender
-    END;
+    END;                                            -- Update Gender column using case function
 ALTER TABLE `Excel Bikes`.`excel project dataset 2 copy`
-DROP COLUMN Gender_Converted;
+DROP COLUMN Gender_Converted;                        -- Deleting the created column
 SELECT 
     CASE 
         WHEN `Marital Status` = 'M' THEN 'Married'
         WHEN `Marital Status` = 'S' THEN 'Single'
         ELSE `Marital Status`
-    END AS Marital_Status_Converted,
-    `Marital Status`
-FROM `Excel Bikes`.`excel project dataset 2 copy`;
+    END AS Marital_Status_Converted,              
+FROM `Excel Bikes`.`excel project dataset 2 copy`;    -- Create new column and insert Marital Status column variables as 'Married' & 'Single'
 UPDATE `Excel Bikes`.`excel project dataset 2 copy`
 SET `Marital Status` = 
     CASE 
         WHEN `Marital Status` = 'M' THEN 'Married'
         WHEN `Marital Status` = 'S' THEN 'Single'
         ELSE `Marital Status`
-    END;
+    END;                                             -- Update Marital Status column using case statement
 
 ALTER TABLE `Excel Bikes`.`excel project dataset 2 copy`
-ADD COLUMN Age_Brackets VARCHAR(50);
+ADD COLUMN Age_Brackets VARCHAR(50);                    -- create new column
 
 UPDATE `Excel Bikes`.`excel project dataset 2 copy`
 SET Age_Brackets =
@@ -43,15 +42,9 @@ SET Age_Brackets =
         WHEN Age >= 31 AND Age <= 54 THEN 'Middle Age'
         WHEN Age > 54 THEN 'Old'
         ELSE 'Unknown'  -- Or any default value if age is NULL or unexpected
-    END;
+    END;                                            -- Use Case function to set Parameters for different age brackets within new column. 
     
-SELECT 
-    e1.`Marital Status` AS Marital_Status,
-    e1.`Purchased Bike` AS Purchased_Bike,
-    e1.`Home Owner` AS Home_Owner,
-    e1.`Commute Distance` AS Commute_Distance
-FROM `Excel Bikes`.`excel project dataset 2 copy` e1
-JOIN `Excel Bikes`.`excel project dataset 2 copy` e2 ON e1.ID = e2.ID;
+
 
 
 
