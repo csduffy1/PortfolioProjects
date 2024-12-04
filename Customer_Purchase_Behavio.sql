710f7df295fbbf666ee793113df07c9f4dd3c81b
Christian McDuffy <csduffy1@gmail.com>
2024-12-04 09:50:02 -0500

    Update and rename Customer_Purchase_Behavior to Customer_Purchase_Behavio.sql

--- Customer_Purchase_Behavio.sql
+++ Customer_Purchase_Behavio.sql
@@ -1,4 +1,4 @@
-
+--Remove Duplicates
 DELETE FROM Transactions
 WHERE TransactionID IN (
     SELECT TransactionID
@@ -9,10 +9,12 @@ WHERE TransactionID IN (
     WHERE RowNum > 1
 );
 
+-- Replace Missing Values
 UPDATE Customers
 SET Country = 'Unknown'
 WHERE Country IS NULL;
 
+-- Fix inconsistent entries in the Gender column:
 UPDATE Customers
 SET Gender = CASE 
     WHEN LOWER(Gender) IN ('m', 'male') THEN 'Male'
@@ -20,19 +22,23 @@ SET Gender = CASE
     ELSE 'Unknown'
 END;
 
+--Delete rows with negative amounts:
 DELETE FROM Transactions
 WHERE Amount < 0;
 
+--Total Revenue by Product Category
 SELECT ProductCategory, SUM(Amount) AS TotalRevenue
 FROM Transactions
 GROUP BY ProductCategory
 ORDER BY TotalRevenue DESC;
 
+--Monthly Revenue Trend
 SELECT DATE_TRUNC('month', PurchaseDate) AS Month, SUM(Amount) AS MonthlyRevenue
 FROM Transactions
 GROUP BY DATE_TRUNC('month', PurchaseDate)
 ORDER BY Month;
 
+-- Top 5 Customers by Spending
 SELECT c.CustomerID, c.Name, SUM(t.Amount) AS TotalSpending
 FROM Customers c
 JOIN Transactions t ON c.CustomerID = t.CustomerID
@@ -40,11 +46,13 @@ GROUP BY c.CustomerID, c.Name
 ORDER BY TotalSpending DESC
 LIMIT 5;
 
+-- Purchase Distribution by Gender
 SELECT c.Gender, COUNT(t.TransactionID) AS NumberOfPurchases, SUM(t.Amount) AS TotalSpent
 FROM Customers c
 JOIN Transactions t ON c.CustomerID = t.CustomerID
 GROUP BY c.Gender;
 
+-- Most Popular Product Categories
 SELECT ProductCategory, COUNT(*) AS TotalPurchases
 FROM Transactions
 GROUP BY ProductCategory
